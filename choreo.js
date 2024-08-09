2e1ac599f8f20cb9064ab41df50b647ee0364d91
Arnish Shakil <arnish.shakil@gmail.com>
2024-08-09 17:51:57 -0400

    added onclick function

--- choreo.js
+++ choreo.js
@@ -1,2 +1,6 @@
 const theLib = [];
-
+const addBookBtn = document.getElementById("addBook");
+addBookBtn.addEventListener("click", function () {
+    document.getElementById("modalBG").style.display = "flex";
+    document.getElementById("modalContent").style.display = "flex";
+});
